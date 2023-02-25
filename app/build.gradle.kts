209c8a84313732020684375448c8ef3cf09d1370
heshmattawfik <heshmattawfik@gmail.com>
2023-02-25 22:56:34 +0100

    - Create Countries.graphql - Create Country.graphql

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("com.apollographql.apollo3").version("3.7.3")
     kotlin("kapt")
     id("dagger.hilt.android.plugin")
 }
 
+apollo {
+    service("service") {
+        packageName.set("com.heshmat")
+    }
+}
 
 android {
     namespace = Constants.namespace
@@ -60,6 +66,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.44.2")
     kapt("com.google.dagger:hilt-android-compiler:2.44.2")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("com.apollographql.apollo3:apollo-runtime:3.7.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
